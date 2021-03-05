ac76af47520134644c91e74d7888ca554e68e2ca
sefademirkiran <64711131+sefademirkiran@users.noreply.github.com>
2021-03-05 22:09:45 +0300

    .

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -22,5 +22,27 @@ namespace Business.Constants
         public static string ColorAdded = "Renk Eklendi";
         public static string ColorDeleted = "Renk Silindi";
         public static string ColorUpdeted = "Renk Güncellendi.";
+
+        public static string UserAdded = "Kullanıcı Eklendi";
+
+        public static string UserDeleted = "Kullanıcı Silindi. ";
+
+        public static string UserUpdated = "Kullanıcı Güncellendi";
+
+        public static string CustomerAdded = "Müşteri Eklendi";
+
+        public static string CustomerDeleted = "Müşteri Silindi";
+
+        public static string CustomerUpdated = "Müşteri Güncellendi";
+
+        public static string CustomerListed = "Müşteri Listelendi";
+
+        public static string RentalAdded = "Kiralama Eklendi";
+
+        public static string RentalDeleted = "Kiralama Silindi ";
+
+        public static string RentalUpdated = "Kiralama Güncellendi";
+
+        public static string RentalListed { get; internal set; }
     }
 }
---
+++ DataAccess/Concrete/EntityFramework/Repository/EfRentalDal.cs
@@ -0,0 +1,57 @@
+﻿using Core.DataAccess.EntitiyFramework;
+using DataAccess.Abstract;
+
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using DataAccess.Concrete.EntityFramework.Context;
+
+namespace DataAccess.Concrete.EntityFramework.Repository
+{
+    public class EfRentalDal : EfEntityRepositoryBase<Rental, RentACarContext>, IRentalDal
+    {
+        //    public List<RentalDetailDto> GetRentalDetails()
+        //    {
+        //        using (RentACarContext context = new RentACarContext())
+        //        {
+        //            var result = from rental in context.Rental
+        //                         join car in context.Cars on rental.CarId equals car.Id
+        //                         join customer in context.Customers on rental.UserId equals customer.UserId
+        //                         join user in context.Users on customer.Id equals user.Id
+        //                         join brand in context.Brands on car.BrandId equals brand.BrandId
+        //                         join color in context.Colors on car.ColorId equals color.ColorId
+        //                         select new RentalDetailDto
+        //                         {
+
+        //                             RentalId = rental.RentalId,
+        //                             CarId = car.Id,
+        //                             CustomerId = customer.CustomerId,
+        //                             UserId = user.Id,
+        //                             CompanyName = customer.CompanyName,
+        //                             BrandName = brand.BrandName,
+        //                             ColorName = color.ColorName,
+        //                             FirstName = user.FirstName,
+        //                             LastName = user.LastName,
+        //                             //RentDate = rental.RentDate,
+        //                             //ReturnDate = rental.ReturnDate,
+        //                             ModelYear = car.ModelYear,
+        //                             DailyPrice = car.DailyPrice,
+        //                             Description = car.Descriptions,
+
+        //                         };
+        //            return result.ToList();
+
+        //        }
+        //    }
+        //}
+        public List<RentalDetailDto> GetRentalDetails()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+    
+
